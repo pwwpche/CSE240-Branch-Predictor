113b05e46a7e89d80c74928faffb0a785aaae162
pwwpche <pwwpche@gmail.com>
2017-06-01 18:48:57 -0700

    final update

--- tage.h
+++ tage.h
@@ -13,7 +13,8 @@
 #define NOTTAKEN  0
 #define TAKEN     1
 
-/*  Implementation of TAGE Predictor based on:
+/*
+ * Implementation of TAGE Predictor based on:
  *  A case for (partially)-tagged geometric history length predictors
  *  Andre Seznec, IRISA
  *  http://www.irisa.fr/caps/people/seznec/JILP-COTTAGE.pdf
@@ -22,31 +23,63 @@
  *
  *  Configuration:
  *
- *  1. 1x basic bimodal predictor, There are BIMODAL_SIZE = 3000 entries in it.
+ *  1. 1x basic bimodal predictor, There are BIMODAL_SIZE = 4099 entries in it.
  *     Each entry is a 2-bit saturate counter.
+ *     #define BIMODAL_SIZE 4099
+ *     #define LEN_BIMODAL 2
+ *     int8_t t_bimodalPredictor[BIMODAL_SIZE];    // Bimodal Predictor table
  *     Size for bimodal predictor: 2 * (4099) = 8198 bits.
  *
  *  2. 7x TAGE tables
- *     (1 << LEN_GLOBAL) = (1 << 9) = 512 items for each table
- *     Each entry has a 3 bit saturate counter, 2 bit usefulness counter, 10 bit tag.
- *     Total length : 3 + 2 + 10 = 15 bit
+ *     #define NUM_BANKS 7                      // 7x TAGE tables
+ *     #define LEN_TAG 10                       // 10 bit tag length
+ *     #define LEN_GLOBAL 9                     // index into global table in each bank is 9 bit, thus there are 2^9 = 512 entries in each table.
+ *     #define LEN_COUNTS 3                     // Each entry has a 3 bit saturate counter, 2 bit usefulness counter, 10 bit tag.
+ *     typedef struct CompressedStruct{
+            int8_t geometryLength;
+            int8_t targetLength;
+            uint32_t compressed;
+        } CompressedHistory ;                   // 8 + 8 + 32 = 48 bits.
+
+        typedef struct BankEntryStruct{
+            int8_t saturateCounter;             // 3 bit saturate counter
+            uint16_t tag;                       // 10 bit tag
+            int8_t usefulness;                  // 2 bit usefulness counter
+        } BankEntry ;                           // 15 bits in total.
+
+       typedef struct BankStruct{
+            int geometry;                       //This is predefined by GEOMETRICS and never changes, so does not count into total number of bits used.
+            BankEntry entry[1 << LEN_GLOBAL];   // (1 << LEN_GLOBAL) = (1 << 9) = 512 items for each table
+            CompressedHistory indexCompressed;  // 48 bits.
+            CompressedHistory tagCompressed[2]; // 48 * 2 bits.
+        } Bank;
+
+
  *     Total space for entries: 512 * 15 = 7680 bits.
  *     Each table has 3 CompressedHistory, (8 + 8 + 32) = 48 bit.
  *     Total size: 7 * (48 + 15 + 7680) = 54201 bits
  *
  *
  *  3. 1x Global History Table
+ *     #define MAX_HISTORY_LEN 131
+ *     uint8_t t_globalHistory[MAX_HISTORY_LEN];
+ *
  *     MAX_HISTORY_LEN = 131 entries, 1 bit per entry
  *     Total space: 131 bits.
  *
  *  4. BankGlobalIndex:
+ *     uint32_t bankGlobalIndex[NUM_BANKS];
  *     Store the entry index to each bank.
  *     Each entry consumes LEN_GLOBAL = 9 bit.
  *     Total space: 9 * 7 = 63 bits.
  *
+ *  5. tagResult:
+ *     int tagResult[NUM_BANKS];
+ *     Store the 10-bit tag to each bank in last computation.
+ *     Total space: 10 * 7 = 70 bits.
  *
  *  Total size:
- *     8198 + 54201 + 131 + 63 = 62593 bits.
+ *     8198 + 54201 + 131 + 63 + 70 = 62663 bits.
  *
  */
 
@@ -89,7 +122,6 @@ typedef struct BankStruct{
     BankEntry entry[1 << LEN_GLOBAL];
     CompressedHistory indexCompressed;
     CompressedHistory tagCompressed[2];
-
 } Bank;
 
 int8_t t_bimodalPredictor[BIMODAL_SIZE];    // Bimodal Predictor table
@@ -104,6 +136,7 @@ uint8_t alternatePrediction = NOTTAKEN;
 uint8_t lastPrediction = NOTTAKEN;
 
 uint32_t bankGlobalIndex[NUM_BANKS];
+int tagResult[NUM_BANKS];
 
 int8_t useAlternate = 8;
 
@@ -221,7 +254,7 @@ void tage_init(){
 
 uint8_t tage_predict(uint32_t pc){
 
-    int tagResult[NUM_BANKS];
+
 
     // Get tag in each bank and indices for entry in each bank
     for(uint32_t i = 0 ; i < NUM_BANKS ; i++){
